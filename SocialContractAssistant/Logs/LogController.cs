a00cc98c12b4ad0284f5f96761ef56d73712da3e
Кирилл Максимов <kirill-kaluga@mail.ru>
2023-04-07 23:56:31 +0300

    Added log

---
+++ SocialContractAssistant/Logs/LogController.cs
@@ -0,0 +1,30 @@
+﻿using SocialContractAssistant.DataAccess;
+using SocialContractAssistant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialContractAssistant.Logs
+{
+    internal class LogController
+    {
+        public void Error(String _class, String _method, String _message, Int64 chatId = 0)
+        {
+            using (var db = new DataBaseContext())
+            {
+                var log = new LogModel();
+
+                log.CreateDate= DateTime.Now;
+                log.Class = _class;
+                log.Method = _method;
+                log.Message = _message;
+                log.ChatId = chatId;
+
+                db.Logs.Add(log);
+                db.SaveChanges();
+            }
+        }
+    }
+}
